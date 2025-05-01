ce5f7e8ad0a94917f19f5ebcb3781cfa571913f1
Ma Feilong <mafeilong@gmail.com>
2025-05-01 12:48:13 -0400

    Remove the print statement, which was for debugging.

--- src/smriprep/workflows/surfaces.py
+++ src/smriprep/workflows/surfaces.py
@@ -1386,7 +1386,7 @@ def init_resample_surfaces_wb_wf(
         iterfield=['surface_in', 'current_sphere', 'new_sphere'],
         name='resampler',
     )
-    new_sphere = [
+    resampler.inputs.new_sphere = [
         str(
             tf.get(
                 template=space,
@@ -1401,8 +1401,6 @@ def init_resample_surfaces_wb_wf(
         for _surf in surfaces
         for hemi in ['L', 'R']
     ]
-    print(new_sphere)
-    resampler.inputs.new_sphere = new_sphere
 
     surface_groups = pe.Node(
         niu.Split(splits=[2] * len(surfaces)),
